f28121a1c91c60238cef75131a2978724dcc868c
rahrindra <rahrindra@gmail.com>
2016-11-18 15:45:59 +0300

    utilisation de plusieurs controlleurs

---
+++ src/controllers/categoryController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+app.
+	controller('categoryIndex', function($scope) {
+
+	})
+	.controller('categoryList', function($scope) {
+
+	})
+	.controller('categoryCreate', function($scope) {
+		$scope.test = 'toto';
+	})
+	.controller('categoryRemove', function($scope) {
+
+	})
+;
---
+++ src/controllers/itemController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+app.
+	controller('itemIndex', function($scope) {
+
+	})
+	.controller('itemList', function($scope) {
+		
+	})
+	.controller('itemCreate', function($scope) {
+		
+	})
+	.controller('itemRemove', function($scope) {
+		
+	})
+;
