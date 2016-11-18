b4f449bcf71090f7a59d77a1605ccaf6c4a4718b
rahrindra <rahrindra@gmail.com>
2016-11-18 16:27:51 +0300

    ajout des modèles de données

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var categories = [
+	{'id': 1, 'name': 'films'},
+	{'id': 2, 'name': 'musiques'}
+];
+
 app.
 	controller('categoryIndex', function($scope) {
 
--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var items = [
+	{'name': 'film 1', 'categorie_id': 1},
+	{'name': 'film 2', 'categorie_id': 2},
+	{'name': 'music 1', 'categorie_id': 2}
+];
+
 app.
 	controller('itemIndex', function($scope) {
 
